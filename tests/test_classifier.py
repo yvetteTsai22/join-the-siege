fa8323d6d1e991199a1ddc5a0259062ba8b93c2d
Yvette <yvette.tsai22@gmail.com>
2024-11-13 16:53:20 +0000

    ✅test: add tests for classifier

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -1,15 +1,28 @@
 # to test classifier.py
 import pytest
 import pandas as pd
-from src.classifier import XGBoostClassifier
+from src.classifier import XGBoostClassifier, XGBoostClassifierSemantic
 
 
 @pytest.fixture
 def classifier():
-    return XGBoostClassifier()
+    # return XGBoostClassifier()
+    return XGBoostClassifierSemantic()
 
 
 @pytest.mark.asyncio
 async def test_predict(classifier):
     result = await classifier.predict("invoice_010.pdf")
     assert result == "invoice"
+
+
+@pytest.mark.asyncio
+async def test_predict2(classifier):
+    result = await classifier.predict("bank_statement_1.pdf")
+    assert result == "bank_statement"
+
+
+@pytest.mark.asyncio
+async def test_predict3(classifier):
+    result = await classifier.predict("drivers_license_2.pdf")
+    assert result == "driver_license"
