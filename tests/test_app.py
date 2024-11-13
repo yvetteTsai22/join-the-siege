8528844be5e45ce4550428cfd6645ae3e16e2e6f
Yvette <yvette.tsai22@gmail.com>
2024-11-13 00:43:57 +0000

    ✅test: modify tests for fastAPI

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,37 +2,38 @@ from io import BytesIO
 
 import pytest
 from src.app import app, allowed_file
+from fastapi.testclient import TestClient
+
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
+    client = TestClient(app)
+    yield client
 
 
-@pytest.mark.parametrize("filename, expected", [
-    ("file.pdf", True),
-    ("file.png", True),
-    ("file.jpg", True),
-    ("file.txt", False),
-    ("file", False),
-])
+@pytest.mark.parametrize(
+    "filename, expected",
+    [
+        ("file.pdf", True),
+        ("file.png", True),
+        ("file.jpg", True),
+        ("file.txt", False),
+        ("file", False),
+    ],
+)
 def test_allowed_file(filename, expected):
     assert allowed_file(filename) == expected
 
-def test_no_file_in_request(client):
-    response = client.post('/classify_file')
-    assert response.status_code == 400
 
 def test_no_selected_file(client):
-    data = {'file': (BytesIO(b""), '')}  # Empty filename
-    response = client.post('/classify_file', data=data, content_type='multipart/form-data')
+    response = client.post("/classify_file")
     assert response.status_code == 400
 
-def test_success(client, mocker):
-    mocker.patch('src.app.classify_file', return_value='test_class')
 
-    data = {'file': (BytesIO(b"dummy content"), 'file.pdf')}
-    response = client.post('/classify_file', data=data, content_type='multipart/form-data')
+def test_success(client, mocker):
+    mocker.patch("src.app.classifier.predict", return_value="invoice")
+
+    files = {"file": ("test.pdf", BytesIO(b"test file content"))}
+    response = client.post("/classify_file", files=files)
     assert response.status_code == 200
-    assert response.get_json() == {"file_class": "test_class"}
+    assert response.json() == {"file_class": "test_class"}
