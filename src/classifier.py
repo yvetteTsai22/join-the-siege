d674cff938f927fcb53161e4876e3ef6c992ad08
Yvette <yvette.tsai22@gmail.com>
2024-11-13 16:45:58 +0000

    ♻️feat: clean code

--- src/classifier.py
+++ src/classifier.py
@@ -18,10 +18,18 @@ def classify_file(file: FileStorage):
     return "unknown file"
 
 
+import re
+
+
+def clean_text(text):
+    # Remove special characters and replace underscores with spaces
+    return re.sub(r"[^a-zA-Z0-9\s]", "", text.replace("_", " "))
+
+
 class XGBoostClassifier:
     def __init__(self):
         self.model = None
-        self.tfidf_vectorizer = None
+        self.vectorizer = None
         self.label_encoder = None
 
         # check if model and encoder exists
@@ -32,7 +40,7 @@ class XGBoostClassifier:
             os.path.dirname(__file__), ".", "model", "model.joblib"
         )
         vectorizer_path = os.path.join(
-            os.path.dirname(__file__), ".", "model", "tfidf_vectorizer.pkl"
+            os.path.dirname(__file__), ".", "model", "vectorizer.pkl"
         )
         label_encoder_path = os.path.join(
             os.path.dirname(__file__), ".", "model", "label_encoder.pkl"
@@ -44,7 +52,7 @@ class XGBoostClassifier:
         ):
             self.model = joblib.load(model_path)
             with open(vectorizer_path, "rb") as f:
-                self.tfidf_vectorizer = pickle.load(f)
+                self.vectorizer = pickle.load(f)
             with open(label_encoder_path, "rb") as f:
                 self.label_encoder = pickle.load(f)
         else:
@@ -68,28 +76,26 @@ class XGBoostClassifier:
             raise ValueError("test data has more categories than training data")
         return df_train, df_test
 
+    def prepare_data(self, df_train, df_test):
+        X_train, y_train = self.vectorizer.fit_transform(
+            df_train["filename"].apply(clean_text)
+        ).toarray(), self.label_encoder.fit_transform(df_train["category"])
+        X_test, y_test = self.vectorizer.transform(
+            df_test["filename"].apply(clean_text)
+        ).toarray(), self.label_encoder.transform(df_test["category"])
+        return X_train, y_train, X_test, y_test
+
     def train_model(self, df_train, df_test):
 
         from sklearn.feature_extraction.text import TfidfVectorizer
         from sklearn.preprocessing import LabelEncoder
 
-        self.tfidf_vectorizer = TfidfVectorizer(
+        self.vectorizer = TfidfVectorizer(
             max_features=100, stop_words="english", max_df=0.95, min_df=2
         )
         self.label_encoder = LabelEncoder()
 
-        import re
-
-        def clean_text(text):
-            # Remove special characters and replace underscores with spaces
-            return re.sub(r"[^a-zA-Z0-9\s]", "", text.replace("_", " "))
-
-        X_train, y_train = self.tfidf_vectorizer.fit_transform(
-            df_train["filename"].apply(clean_text)
-        ).toarray(), self.label_encoder.fit_transform(df_train["category"])
-        X_test, y_test = self.tfidf_vectorizer.transform(
-            df_test["filename"].apply(clean_text)
-        ).toarray(), self.label_encoder.transform(df_test["category"])
+        X_train, y_train, X_test, y_test = self.prepare_data(df_train, df_test)
 
         import xgboost as xgb
         from sklearn.metrics import accuracy_score, f1_score
@@ -117,6 +123,11 @@ class XGBoostClassifier:
         print(f"Accuracy: {accuracy:.4f}")
         print(f"F1 Score: {f1:.4f}")
 
+        self.store_model()
+
+        return {"accuracy": accuracy, "f1_score": f1}
+
+    def store_model(self):
         # save model
         import joblib
         import os
@@ -126,22 +137,20 @@ class XGBoostClassifier:
         )
         joblib.dump(self.model, model_path)
         # Save the TF-IDF vectorizer and label encoder
-        tfidf_vectorizer_path = os.path.join(
-            os.path.dirname(__file__), ".", "model", "tfidf_vectorizer.pkl"
+        vectorizer_path = os.path.join(
+            os.path.dirname(__file__), ".", "model", "vectorizer.pkl"
         )
         label_encoder_path = os.path.join(
             os.path.dirname(__file__), ".", "model", "label_encoder.pkl"
         )
-        with open(tfidf_vectorizer_path, "wb") as f:
-            pickle.dump(self.tfidf_vectorizer, f)
+        with open(vectorizer_path, "wb") as f:
+            pickle.dump(self.vectorizer, f)
         with open(label_encoder_path, "wb") as f:
             pickle.dump(self.label_encoder, f)
 
-        return {"accuracy": accuracy, "f1_score": f1}
-
     async def predict(self, filename):
         # Convert the filename to the same feature format as the training data
-        tfidf_features = self.tfidf_vectorizer.transform([filename]).toarray()
+        tfidf_features = self.vectorizer.transform([filename]).toarray()
         # Predict the category
         prediction = self.model.predict(tfidf_features)[0]
         return self.label_encoder.inverse_transform([prediction])[0]
@@ -164,10 +173,10 @@ def get_embeddings(texts):
     return embeddings.embed_documents(texts)
 
 
-class XGBoostClassifierSemantic:
+class XGBoostClassifierSemantic(XGBoostClassifier):
     def __init__(self):
         self.model = None
-        self.vectorizer = embeddings
+        self.vectorizer = None
         self.label_encoder = None
 
         # check if model and encoder exists
@@ -190,67 +199,16 @@ class XGBoostClassifierSemantic:
             )
             self.train_model(df_train=df_train, df_test=df_test)
 
-    def load_data(
-        self,
-        training_data_file="data/train_data.csv",
-        testing_data_file="data/test_data.csv",
-    ):
-        import pandas as pd
-
-        # Load the data
-        df_train = pd.read_csv(training_data_file)
-        df_test = pd.read_csv(testing_data_file)
-        # ensure test has fewer or equal category as training
-        if len(df_test["category"].unique()) > len(df_train["category"].unique()):
-            raise ValueError("test data has more categories than training data")
-        return df_train, df_test
-
-    def train_model(self, df_train, df_test):
-
-        from sklearn.feature_extraction.text import TfidfVectorizer
-        from sklearn.preprocessing import LabelEncoder
-
-        self.label_encoder = LabelEncoder()
-
-        import re
-
-        def clean_text(text):
-            # Remove special characters and replace underscores with spaces
-            return re.sub(r"[^a-zA-Z0-9\s]", "", text.replace("_", " "))
-
+    def prepare_data(self, df_train, df_test):
         X_train, y_train = get_embeddings(
-            df_train["filename"].apply(clean_text).tolist()
+            df_train["filename"].tolist()
         ), self.label_encoder.fit_transform(df_train["category"])
         X_test, y_test = get_embeddings(
-            df_test["filename"].apply(clean_text).tolist()
+            df_test["filename"].tolist()
         ), self.label_encoder.transform(df_test["category"])
+        return X_train, y_train, X_test, y_test
 
-        import xgboost as xgb
-        from sklearn.metrics import accuracy_score, f1_score
-
-        # Initialize XGBoost model
-        self.model = xgb.XGBClassifier(
-            objective="multi:softmax",
-            num_class=len(self.label_encoder.classes_),
-            reg_alpha=0.1,
-            reg_lambda=0.1,
-            n_estimators=1000,
-            max_depth=15,
-            learning_rate=0.05,
-        )
-
-        # Train the model
-        self.model.fit(X_train, y_train)
-
-        # Make predictions
-        y_pred = self.model.predict(X_test)
-
-        # Evaluate accuracy
-        accuracy = accuracy_score(y_test, y_pred)
-        f1 = f1_score(y_test, y_pred, average="weighted")
-        print(f"Accuracy: {accuracy:.4f}")
-        print(f"F1 Score: {f1:.4f}")
-
+    def store_model(self):
         # save model
         import joblib
         import os
@@ -265,8 +223,6 @@ class XGBoostClassifierSemantic:
         )
         with open(label_encoder_path, "wb") as f:
             pickle.dump(self.label_encoder, f)
-
-        return {"accuracy": accuracy, "f1_score": f1}
 
     async def predict(self, filename):
         # Convert the filename to the same feature format as the training data
